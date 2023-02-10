e756b0a48558e5a3943483505c75779a0b1c6cf5
Jona-St <js@papoo.de>
2023-02-10 12:17:00 +0100

    Added All

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,85 +1,122 @@
 import React, {useCallback, useState} from "react";
 
-import {Card, TextContainer, TextField,} from "@shopify/polaris";
-
-import {makeStyles} from "@material-ui/core/styles";
-import XRegExp from "xregexp";
+import {Button, Card, Form, TextContainer, TextField,FormLayout} from "@shopify/polaris";
 
 
+import { useAuthenticatedFetch } from "../hooks";
 
-function prepareLb(lb) {
-    // Allow mode modifier before lookbehind
-    var parts = /^((?:\(\?[\w$]+\))?)\(\?<([=!])([\s\S]*)\)$/.exec(lb);
-    return {
-        // $(?!\s) allows use of (?m) in lookbehind
-        lb: XRegExp(parts ? parts[1] + "(?:" + parts[3] + ")$(?!\\s)" : lb),
-        // Positive or negative lookbehind. Use positive if no lookbehind group
-        type: parts ? parts[2] === "=" : !parts
-    };
-}
 
-XRegExp.execLb = function (str, lb, regex) {
-    var pos = 0, match, leftContext;
-    lb = prepareLb(lb);
-    while (match = XRegExp.exec(str, regex, pos)) {
-        leftContext = str.slice(0, match.index);
-        if (lb.type === lb.lb.test(leftContext)) {
-            return match;
+
+/*
+Component for the Textfield
+
+ */
+const ValidationTextField = ()=>{
+
+  const [helperText,setHelperText] = useState('');
+  const [error, setError] = useState(false);
+  const [value, setValue] = useState('');
+
+  const handleChange = useCallback((newValue)=>setValue(newValue),[]);
+
+  const fetch = useAuthenticatedFetch();
+
+  //Handles the button
+  const handleSubmit = useCallback(async (event) => {
+      event.preventDefault();
+
+      const regexConst = /<script\s+src="(https?:\/\/[^\/]+\/public\/(ccm19|app)\.js\?[^"]+)"\s+referrerpolicy="origin">\s*<\/script>/;
+      if (!regexConst.test(value)) {
+          setHelperText('The script provided is not valid. Please insert a valid script or contact Support.');
+          setError(true);
+          return;
+      }
+
+      try {
+          const response = await saveScript(value);
+          console.log(response);
+      }catch (e) {
+          console.log("script couldn't be saved");
+      }
+
+        try{
+          const insertScript = await modifyTemplate();
+          console.log(insertScript);
+        }catch (e) {
+            console.log("Template couldn't be modified")
         }
-        pos = match.index + 1;
+
+  }, [value]);
+
+
+    const saveScript = async (value) => {
+        const response = await fetch('/api/script/save', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({value}),
+        });
+
     }
-    return null;
-};
+    const getScript = async () =>{
+      try {
+          const response = await fetch('/api/script/load',{
+              method: 'GET',
+              headers:{
+                  'Content-Type': 'application/json'
+              }
+          });
 
-XRegExp.matchAllLb = function (str, lb, regex) {
-    var matches = [], pos = 0, match, leftContext;
-    lb = prepareLb(lb);
-    while (match = XRegExp.exec(str, regex, pos)) {
-        leftContext = str.slice(0, match.index);
-        if (lb.type === lb.lb.test(leftContext)) {
-            matches.push(match[0]);
-            pos = match.index + (match[0].length || 1);
-        } else {
-            pos = match.index + 1;
-        }
+          const script = await response.json();
+          console.log(script);
+          return script;
+      }catch (error){
+          console.log("couldn't retrieve script from DB")
+      }
     }
-    return matches;
-};
 
-
-const useStyles = value =>
-    makeStyles(theme => ({
-        root: {
-            "& .Mui-error": {
-                color: acquireValidationColor(value)
+    const getTemplate = async () => {
+        const templateResponse = await fetch('/admin/themes/current.json', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Shopify-Access-Token": "<SHOPIFY_API_KEY>"
             },
-            "& .MuiFormHelperText-root": {
-                color: acquireValidationColor(value)
-            }
-        }
-    }));
-
-const acquireValidationColor = message => {
-    switch (message) {
-        case "Wrong or false script pleas reenter or contact support":
-            return "red";
-        case "Please input":
-            return "orange";
-        default:
-            return "black";
+        });
     }
-};
 
-const ValidationTextField = ({ helperText }) => {
+    const saveTemplate = async (templateData) => {
+        const saveTemplateResponse = await fetch('/admin/themes/current.json', {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Shopify-Access-Token": "<SHOPIFY_API_KEY>"
+            },
+            body: JSON.stringify(templateData),
+        });
 
-    const classes = useStyles(helperText)();
+    }
 
-    const [value, setValue] = useState('');
-    const handleChange = useCallback((newValue)=>setValue(newValue),[]);
 
-   // const error = handleError(value) ?  "invalid Snippet pleas check your input or contact support" : "";
-    const error = "arr";
-    return (
+    const modifyTemplate = async () => {
+
+        const templateData= await getTemplate();
+        const script = await getScript();
+
+        const headIndex = templateData.indexOf("<Head>");
+        const headCloseIndex = templateData.indexOf("</Head>");
+
+        const saveTemplate = saveTemplate( templateData.substring(0,headIndex + 6)+script +
+        templateData.substring(headIndex +6,headCloseIndex) +
+        templateData.substring(headCloseIndex));
+
+
+        return ("success");
+    }
+
+
+  // const error = handleError(value) ?  "invalid Snippet pleas check your input or contact support" : "";
+  return (
+      <Form onSubmit={handleSubmit}>
         <TextField
             label="Pleas insert your, from the devine Backend given, CCM19 Skript."
             placeholder="<script src=http://site/public/app.js?apiKey=1337753522109847&amp;domain=1337  referrerpolicy=origin></script>"
@@ -92,52 +129,31 @@ const ValidationTextField = ({ helperText }) => {
             helperText={helperText}
             autoComplete="off"
         />
-
-    );
+        <Button submit>Save</Button>
+      </Form>
+  );
 
 };
-/*
-export function handleError(script){
-    console.log(script);
-    return !!decipher(script);
-}
 
-//todo fix or outsource to php regex pattern
- export function decipher(script) {
 
-    const regexConst = new RegExp('/\bsrc=([\'"])((?=[^"\'?#]|(?!\1)["\'])*\/(ccm19|app)\.js\?(?=[^"\']|(?!\1).)*)\1');
-    if (script.trim() !== "") {
-        let matches = script.match(regexConst);
-          if (matches[1] && matches[2]) {
-              return decodeURI(script);
 
-          }
-        return null;
-
-    }
-}
-*/
 export default function Homepage() {
-    return (
-        <div className="HomePage">
-            <Card primaryFooterAction={{
-                content: 'Save', onAction() {
-                }
-            }}>
-                <Card.Header actions={[{content: 'CCM19'}]} title="CCM19 Integration"></Card.Header>
-                <Card.Section>
-                    <TextContainer>
-                        This ist the official CCM19 Integration App. For USage you will need the CCM19 Script from
-                        your CCM19 Backend.
-                    </TextContainer>
-                </Card.Section>
-                <Card.Section title="">
-                    <>
-                        <ValidationTextField/>
-                    </>
-                </Card.Section>
-            </Card>
-        </div>
-    );
+
+  return (
+      <div className="HomePage">
+        <Card.Header actions={[{content: 'CCM19'}]} title="CCM19 Integration"></Card.Header>
+        <Card.Section>
+          <TextContainer>
+            This ist the official CCM19 Integration App. For USage you will need the CCM19 Script from
+            your CCM19 Backend.
+          </TextContainer>
+        </Card.Section>
+        <Card.Section title="">
+          <>
+            <ValidationTextField/>
+          </>
+        </Card.Section>
+      </div>
+  );
 }
 
---
+++ web/middleware/ccm19-script-api.js
@@ -0,0 +1,41 @@
+import express from "express";
+import {ScriptDb} from "../ccm19-script-db.js";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+export default function applyScriptApiEndpoints(app) {
+    app.use(express.json());
+    app.use(bodyParser.json());
+
+    app.post('/api/script/save', async (req, res) => {
+        try {
+            const {value} = req.body;
+            const existingScript = await ScriptDb.read(1);
+            let scriptId;
+            if (existingScript) {
+                await ScriptDb.update(1, {script: value});
+                scriptId = existingScript.id;
+            } else {
+                scriptId = await ScriptDb.create({script: value});
+            }
+            res.send({status: 'success', scriptId});
+        } catch (error) {
+            res.status(500).send({status: 'error', message: error.message});
+        }
+    });
+
+    app.get('/api/script/load', async (req,res) =>{
+        try{
+        const script = await ScriptDb.read(1);
+        return res.send(script.script);
+        }catch(error){
+            return res.status(404).send({status:'error',message: error.message});
+
+        }
+    });
+
+}
