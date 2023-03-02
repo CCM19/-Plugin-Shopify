4dd85f717e5ccf3990c0c487b0659fea999e2247
Jona-St <js@papoo.de>
2023-03-02 23:38:08 +0100

    added translation

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -7,29 +7,32 @@ import {
   QueryProvider,
   PolarisProvider,
 } from "./components";
-
+import {I18nextProvider} from "react-i18next";
+import i18n from "./i18n.js";
 export default function App() {
   // Any .tsx or .jsx files in /pages will become a route
   // See documentation for <Routes /> for more info
   const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
 
   return (
-    <PolarisProvider>
-      <BrowserRouter>
-        <AppBridgeProvider>
-          <QueryProvider>
-            <NavigationMenu
-              navigationLinks={[
-                {
-                  label: "Page name",
-                  destination: "/pagename",
-                },
-              ]}
-            />
-            <Routes pages={pages} />
-          </QueryProvider>
-        </AppBridgeProvider>
-      </BrowserRouter>
-    </PolarisProvider>
+      <I18nextProvider i18n={i18n}>
+        <PolarisProvider>
+          <BrowserRouter>
+            <AppBridgeProvider>
+              <QueryProvider>
+                <NavigationMenu
+                    navigationLinks={[
+                      {
+                        label: "Page name",
+                        destination: "/pagename",
+                      },
+                    ]}
+                />
+                <Routes pages={pages}/>
+              </QueryProvider>
+            </AppBridgeProvider>
+          </BrowserRouter>
+        </PolarisProvider>
+      </I18nextProvider>
   );
 }
