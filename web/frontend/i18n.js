4b32b81939370ded218e1beabd117eec1ade6550
Jona-St <js@papoo.de>
2023-03-02 23:38:08 +0100

    added translation

--- web/frontend/i18n.js
+++ web/frontend/i18n.js
@@ -4,6 +4,18 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 const resources = {
+    de: {
+        translation: {
+            form: {
+                field: {
+                    label: 'Bitte geben Sie hier den Code-Schnipsel vom CCM19 Backend ein.',
+                    button: 'Speichern',
+                    homepage: ' Willkommen in der offizielen App zur CCM19 Integration in Shopify. Zur benutzung bedarf es nur den Code-Schnipsel aus dem CCM19 Backend. Sie haben noch keinen? Dann registrieren sie sich jetzt unter:',
+                    link:"https://www.ccm19.de/",
+                },
+            },
+        },
+    },
     en: {
         translation: {
             form: {
@@ -16,23 +28,12 @@ const resources = {
             },
         },
     },
-    ger: {
-        translation: {
-            form: {
-                field: {
-                    label: 'Bitte geben Sie hier den Code-Schnipsel vom CCM19 Backend ein.',
-                    button: 'Speichern',
-                    homepage: ' Willkommen in der offizielen App zur CCM19 Integration in Shopify. Zur benutzung bedarf es nur den COde Schnipsel aus dem CCM19 Backend. Sie haben noch keinen? Dann registrieren sie sich jetzt unter:',
-                    link:"https://www.ccm19.de/",
-                },
-            },
-        },
-    },
+
 };
 
 i18n.use(initReactI18next).init({
     resources,
-    lng: 'ger',
+    lng: 'de',
     fallbackLng: 'en',
 });
 
