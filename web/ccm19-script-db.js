6f4253249d39de7e872e4a8d93afc5af08ee0b85
Jona-St <js@papoo.de>
2023-03-13 15:37:51 +0100

    applied eslint style guide

--- web/ccm19-script-db.js
+++ web/ccm19-script-db.js
@@ -1,100 +1,99 @@
-import sqlite3 from "sqlite3";
-import path from "path";
+import sqlite3 from 'sqlite3'
+import path from 'path'
 
-const DEFAULT_DB_FILE = path.join(process.cwd(), "ccm19_script_db.sqlite");
+const DEFAULT_DB_FILE = path.join(process.cwd(), 'ccm19_script_db.sqlite')
 
 export const ScriptDb = {
-    scriptTableName: "ccm19_script",
-    db: null,
-    ready: null,
-    create: async function ({ script }) {
-        await this.ready;
+  scriptTableName: 'ccm19_script',
+  db: null,
+  ready: null,
+  create: async function ({ script }) {
+    await this.ready
 
-        const query = `INSERT INTO ${this.scriptTableName}
+    const query = `INSERT INTO ${this.scriptTableName}
         (script)
         VALUES( ?)
         RETURNING id;
-        `;
-        const rawResults = await this.__query(query, [script]);
-        return rawResults[0].id;
-    },
-    update: async function (id, { script }) {
-        await this.ready;
+        `
+    const rawResults = await this.__query(query, [script])
+    return rawResults[0].id
+  },
+  update: async function (id, { script }) {
+    await this.ready
 
-        const query = `
+    const query = `
         UPDATE ${this.scriptTableName}
         SET
         script = ?
         WHERE
             id = ? ;
-        `;
-        await this.__query(query, [script, id]);
-        return true;
-    },
-    read: async function (id) {
-        await this.ready;
-        const query = `
+        `
+    await this.__query(query, [script, id])
+    return true
+  },
+  read: async function (id) {
+    await this.ready
+    const query = `
         SELECT * FROM ${this.scriptTableName}
         WHERE id = ? ;
-        `;
-        const rows = await this.__query(query, [id]);
-        if (!Array.isArray(rows) || rows ?.length !== 1) {
-            return undefined;
-        }
-        return rows[0];
-    },
+        `
+    const rows = await this.__query(query, [id])
+    if (!Array.isArray(rows) || rows?.length !== 1) {
+      return undefined
+    }
+    return rows[0]
+  },
 
-    delete: async function (id) {
-        await this.ready;
-        const query = `
+  delete: async function (id) {
+    await this.ready
+    const query = `
         DELETE FROM ${this.scriptTableName}
         WHERE id = ? ;
-        `;
-        await this.__query(query, [id]);
-        return true;
-    },
+        `
+    await this.__query(query, [id])
+    return true
+  },
 
-    init: async function () {
-        this.db = this.db ?? new sqlite3.Database(DEFAULT_DB_FILE);
+  init: async function () {
+    this.db = this.db ?? new sqlite3.Database(DEFAULT_DB_FILE)
 
-        const hasScriptTable = await this.__hasScriptTable();
+    const hasScriptTable = await this.__hasScriptTable()
 
-        if (!hasScriptTable) {
-            const query = `
+    if (!hasScriptTable) {
+      const query = `
             CREATE TABLE ${this.scriptTableName} (
             id INTEGER PRIMARY KEY NOT null,
             script VARCHAR(511) NOT null
             );
-            `;
-            await this.__query(query);
+            `
+      await this.__query(query)
+    }
+    this.ready = Promise.resolve()
+  },
+  __query: function (sql, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, result) => {
+        if (err) {
+          reject(err)
+          return
         }
-        this.ready = Promise.resolve();
-    },
-    __query: function (sql, params = []) {
-        return new Promise((resolve, reject) => {
-            this.db.all(sql, params, (err, result) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                resolve(result);
-            });
-        });
-    },
-    __hasScriptTable: async function () {
-        const query = `
+        resolve(result)
+      })
+    })
+  },
+  __hasScriptTable: async function () {
+    const query = `
         SELECT name FROM sqlite_master
         WHERE
         type = 'table' AND
         name = ? ;
-        `;
-        const rows = await this.__query(query, [this.scriptTableName]);
-        return rows.length === 1;
-    },
+        `
+    const rows = await this.__query(query, [this.scriptTableName])
+    return rows.length === 1
+  },
 
-    isConnected: function () {
-        return this.db != null;
-    },
+  isConnected: function () {
+    return this.db != null
+  }
 
-};
-
+}
--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,8 +1,7 @@
-import React, {useCallback, useState} from "react";
-import {Button, Card, Form, InlineError, Link, TextContainer, TextField, TextStyle} from "@shopify/polaris";
-import {useAuthenticatedFetch} from "../hooks";
-import {useTranslation} from "react-i18next";
-
+import React, { useCallback, useState } from 'react'
+import { Button, Card, Form, InlineError, Link, TextContainer, TextField, TextStyle } from '@shopify/polaris'
+import { useAuthenticatedFetch } from '../hooks'
+import { useTranslation } from 'react-i18next'
 
 /**
  * Core component for the handling of the input processing of it.
@@ -10,105 +9,94 @@ import {useTranslation} from "react-i18next";
  * @returns {JSX.Element}
  * @constructor
  */
-const ValidationTextField = ()=>{
-    const fetch = useAuthenticatedFetch();
+const ValidationTextField = () => {
+  const fetch = useAuthenticatedFetch()
 
-    const [success, setSuccess] = useState(false);
-    const [inputError, setInputError] = useState(false);
-    const [inputScript, setInputScript] = useState('');
-    const {t} = useTranslation();
+  const [success, setSuccess] = useState(false)
+  const [inputError, setInputError] = useState(false)
+  const [inputScript, setInputScript] = useState('')
+  const { t } = useTranslation()
 
-    const handleChange = (newValue) => setInputScript(newValue);
+  const handleChange = (newValue) => setInputScript(newValue)
 
+  const handleSubmit = useCallback(async (event) => {
+    event.preventDefault()
 
-    const handleSubmit = useCallback( async (event) => {
-        event.preventDefault();
+    const regexConst = new RegExp(
+      '<script\\s+src="(https?://[^/]+/public/(ccm19|app)\\.js\\?[^"]+)"\\s+referrerpolicy="origin">\\s*</script>'
+    )
+    // checks if script is correct
+    if (!regexConst.test(inputScript)) {
+      setInputError(true)
+      setSuccess(false)
+      return
+    }
 
+    try {
+      await initDB()
+      // console.log(db);
+      await saveScript(encodeURIComponent(inputScript))
+      // console.log(response);
+    } catch (error) {
+      console.error(error)
+      setSuccess(false)
+      setInputError(true)
+    }
+    try {
+      await modifyTemplate()
+      setSuccess(true)
+      setInputError(false)
+      // console.log(insertScript);
+    } catch (error) {
+      setSuccess(false)
+      setInputError(true)
+      console.error(error)
+    }
+  }, [inputScript])
 
-        const regexConst = new RegExp(
-            `<script\\s+src="(https?://[^/]+/public/(ccm19|app)\\.js\\?[^"]+)"\\s+referrerpolicy="origin">\\s*</script>`
-        );
-        //checks if script is correct
-        if (!regexConst.test(inputScript)) {
-            setInputError(true);
-            setSuccess(false)
-            return;
-        }
-
-        try {
-            await initDB();
-            //console.log(db);
-            await saveScript(encodeURIComponent(inputScript));
-           // console.log(response);
-
-
-        } catch (error) {
-            console.error(error);
-            setSuccess(false);
-            setInputError(true);
-
-        }
-        try {
-
-            await modifyTemplate();
-            setSuccess(true);
-            setInputError(false);
-           // console.log(insertScript);
-
-        } catch (error) {
-            setSuccess(false);
-            setInputError(true);
-            console.error(error);
-
-        }
-
-
-    }, [inputScript]);
-
-    /**
+  /**
      * Initialises the Db if not already done
      *
      * @returns {Promise<Response<any, Record<string, any>, number>>}
      */
-    const initDB = async () =>{
-        return await fetch('/api/get/db/status',{
-            method: 'GET',
-            headers:{
-                'Content-Type': 'application/json'
-            }
-        });
-    }
+  const initDB = async () => {
+    return await fetch('/api/get/db/status', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
 
-    /**
+  /**
      * saves the script
      *
      * @param inputScript
      * @returns {Promise<Response<any, Record<string, any>, number>>}
      */
-    const saveScript = async (inputScript) => {
-        return await fetch('/api/script/save', {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({inputScript}),
-        });
-    }
+  const saveScript = async (inputScript) => {
+    return await fetch('/api/script/save', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ inputScript })
+    })
+  }
 
-    /**
+  /**
      * starts the process of template modification
      *
      * @returns {Promise<Response<any, Record<string, any>, number>>}
      */
-    const modifyTemplate = async () => {
-        return await fetch('/api/template/modify', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+  const modifyTemplate = async () => {
+    return await fetch('/api/template/modify', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
 
-    }
-
-    return (
+  return (
         <Form onSubmit={handleSubmit}>
             <TextField
                 label={t('form.field.label')}
@@ -119,43 +107,41 @@ const ValidationTextField = ()=>{
                 onChange={handleChange}
                 required
                 error={inputError}
-             autoComplete={"off"}/>
+             autoComplete={'off'}/>
             {inputError && (
-                <InlineError message={t('form.field.errorMessage')}  fieldID={inputScript}/>
+                <InlineError message={t('form.field.errorMessage')} fieldID={inputScript}/>
             )}
             {success && (
-                <div style={{ color: "green" }}>
-                    {t("form.field.successMessage")}
+                <div style={{ color: 'green' }}>
+                    {t('form.field.successMessage')}
                 </div>
             )}
 
             <Button submit>{t('form.field.button')}</Button>
         </Form>
-    );
-
-};
+  )
+}
 /**
  *  Displays the components
  * @returns {JSX.Element}
  * @constructor
  */
-export default function Homepage() {
-    const { t } = useTranslation();
-    const linkText=t('form.field.homepage');
-    const linkUrl=t('form.field.link');
+export default function Homepage () {
+  const { t } = useTranslation()
+  const linkText = t('form.field.homepage')
+  const linkUrl = t('form.field.link')
 
-    const headerAction={
-        content : "CCM19 Support",
-        url: "https://www.ccm19.de/en/support-request/",
-    }
+  const headerAction = {
+    content: 'CCM19 Support',
+    url: 'https://www.ccm19.de/en/support-request/'
+  }
 
-
-    return (
+  return (
     <div className="HomePage">
         <Card.Header actions={[headerAction]} title="CCM19 Integration"></Card.Header>
         <Card.Section>
           <TextContainer>
-              <TextStyle variation={"subdued"}>
+              <TextStyle variation={'subdued'}>
                   {linkText}
                   <Link url={linkUrl}>{linkUrl}</Link>
               </TextStyle>
@@ -167,6 +153,5 @@ export default function Homepage() {
           </>
         </Card.Section>
       </div>
-  );
+  )
 }
-
