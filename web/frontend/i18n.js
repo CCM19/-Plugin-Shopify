74dd34e32c6b58d2f20c13148fab5678a405d2c0
Jona-St <js@papoo.de>
2023-04-04 12:35:50 +0200

    bugfixxing

--- web/frontend/i18n.js
+++ web/frontend/i18n.js
@@ -1,7 +1,7 @@
 // i18n.js
 
 import i18n from 'i18next';
-import { initReactI18next } from '../../node_modules/react-i18next';
+import { initReactI18next } from 'react-i18next';
 
 const resources = {
     de: {
