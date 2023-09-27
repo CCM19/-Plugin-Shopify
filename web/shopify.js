ee802d0a69264e10c0150e4e18030b46db4eacec
Jona-St <js@papoo.de>
2023-09-27 15:30:53 +0200

    Syntax fixxes

--- web/shopify.js
+++ web/shopify.js
@@ -1,15 +1,16 @@
-import {  LATEST_API_VERSION } from "@shopify/shopify-api";
-import { shopifyApp } from "@shopify/shopify-app-express";
-import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
-import {join} from "path"
+import {LATEST_API_VERSION} from "@shopify/shopify-api";
+import {shopifyApp} from "@shopify/shopify-app-express";
+import {SQLiteSessionStorage} from "@shopify/shopify-app-session-storage-sqlite";
+import {join} from "path";
 import {restResources} from "@shopify/shopify-api/rest/admin/2023-04";
-import {ScriptDB} from "./script-db.js";
 import sqlite3 from "sqlite3";
+
+import {ScriptDB} from "./script-db.js";
 import {billingConfig} from "./billing.js";
 
-//const sessionDb = new SQLiteSessionStorage(dbFile);
+// const sessionDb = new SQLiteSessionStorage(dbFile);
 const database = new sqlite3.Database(join(process.cwd(), "script_db.sqlite"));
-ScriptDB.db =database;
+ScriptDB.db = database;
 await ScriptDB.init();
 
 const apiKey = process.env.SHOPIFY_API_KEY;
