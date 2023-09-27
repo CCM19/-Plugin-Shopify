98a8465ab7ca9fa998aa333acfdb1b3496885949
Jona-St <js@papoo.de>
2023-09-27 15:29:10 +0200

    modified billing plans

--- web/billing.js
+++ web/billing.js
@@ -1,16 +1,16 @@
-import { BillingInterval } from "@shopify/shopify-api";
+import {BillingInterval} from "@shopify/shopify-api";
 
 export const billingConfig = {
-    "free":{
-        amount: 0.0,
-        currencyCode: "EUR",
-        interval: BillingInterval.OneTime,
-        usageTerms: "Free access"
-    },
-    "starter":{
-        amount: 7.90,
-        currencyCode: "EUR",
-        interval: BillingInterval.Every30Days,
-        usageTerms: "Monthly based Subscription",
-    }
-}
+  starter: {
+    amount: 7.90,
+    currencyCode: "EUR",
+    interval: BillingInterval.Every30Days,
+    usageTerms: "Monthly based Subscription",
+  },
+  free: {
+    amount: 0.00,
+    currencyCode: "EUR",
+    interval: BillingInterval.OneTime,
+    usageTerms: "Monthly based Subscription",
+  },
+};
